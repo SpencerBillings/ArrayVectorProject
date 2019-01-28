a1d46faee6ee1b1487b6ef4fc02837080b2294da
Spencer B <spencerbillings2000@gmail.com>
2019-01-28 09:56:07 -0700

    Created project with .gitignore files, and created runner and controller files

---
+++ ArrayVectorProject/Controller/Controller.cpp
@@ -0,0 +1,28 @@
+//
+//  Controller.cpp
+//  ArrayVectorProject
+//
+//  Created by Billings, Spencer on 1/28/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+void Output
+{
+    
+}
+
+void Controller :: start()
+{
+    vector<int> intVec;
+    vector<string> stringVec;
+    
+    intVec.push_back(1);
+    intVec.push_back(2);
+    intVec.push_back(3);
+    
+    stringVec.push_back("String One");
+    stringVec.push_back("String Two");
+    stringVec.push_back("I am very creative.");
+}
